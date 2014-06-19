4c3e7d75a611dace8523cd3ff1818a257870ff99
albiebrown <AlbieBrown@Albies-MacBook-Air-2.local>
2014-06-19 15:06:29 -0500

    Added data to more methods

--- lib/localbitcoins/client/contacts.rb
+++ lib/localbitcoins/client/contacts.rb
@@ -12,7 +12,7 @@ module LocalBitcoins
     end
 
     def message_contact(contact_id, msg)
-      oauth_request(:post, "/api/contact_message_post/#{contact_id}/", {:msg=>msg})
+      oauth_request(:post, "/api/contact_message_post/#{contact_id}/", {:msg=>msg}).data
     end
 
     def dispute_contact(contact_id)
@@ -20,7 +20,7 @@ module LocalBitcoins
     end
 
     def cancel_contact(contact_id)
-      oauth_request(:post, "/api/contact_cancel/#{contact_id}/")
+      oauth_request(:post, "/api/contact_cancel/#{contact_id}/").data
     end
 
     def fund_contact(contact_id)
@@ -28,7 +28,7 @@ module LocalBitcoins
     end
 
     def create_contact(ad_id, amount, message=nil)
-      oauth_request(:post, "/api/contact_create/#{ad_id}/", {:amount=>amount, :message=>message})
+      oauth_request(:post, "/api/contact_create/#{ad_id}/", {:amount=>amount, :message=>message}).data
     end
 
     def contact_info(contact_id)
@@ -45,22 +45,22 @@ module LocalBitcoins
 
     def active_contacts(contact_type = nil)
       contact_type<<'/' if !contact_type.nil? rescue nil
-      oauth_request(:get, "/api/dashboard/#{contact_type}")
+      oauth_request(:get, "/api/dashboard/#{contact_type}").data
     end
 
     def released_contacts(contact_type = nil)
       contact_type<<'/' if !contact_type.nil? rescue nil
-      oauth_request(:get, "/api/dashboard/released/#{contact_type}")
+      oauth_request(:get, "/api/dashboard/released/#{contact_type}").data
     end
 
     def canceled_contacts(contact_type = nil)
       contact_type<<'/' if !contact_type.nil? rescue nil
-      oauth_request(:get, "/api/dashboard/canceled/#{contact_type}")
+      oauth_request(:get, "/api/dashboard/canceled/#{contact_type}").data
     end
 
     def closed_contacts(contact_type = nil)
       contact_type<<'/' if !contact_type.nil? rescue nil
-      oauth_request(:get, "/api/dashboard/closed/#{contact_type}")
+      oauth_request(:get, "/api/dashboard/closed/#{contact_type}").data
     end
   end
 end

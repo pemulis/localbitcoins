d4bcc4de378fe32a4847bea4268b6a2cfe08d9a5
Will Newman <wnewman88@gmail.com>
2014-06-27 10:35:41 -0500

    HTTP Error checking in OAuth requests

--- lib/localbitcoins/request.rb
+++ lib/localbitcoins/request.rb
@@ -11,33 +11,26 @@ module LocalBitcoins
     protected
 
     # Perform an OAuth API request. The client must be initialized 
-    # with a valid OAuth access token. All API requests to 
-    # LocalBitcoins currently require an access token.
+    # with a valid OAuth access token to make requests with this method
     #
     # path   - Request path
-    # params - Parameters hash
+    # body -   Parameters for requests - GET and POST
     #
     def oauth_request(http_method, path, body={})
       raise 'OAuth access token required!' unless @access_token
       params = { :Accept =>'application/json', :access_token => @access_token.token }
       params.merge!(body) if http_method == :get
       resp = @access_token.request(http_method, path, :params => params, :body => body)
+      case resp
+        when Net::HTTPUnauthorized
+          raise LocalBitcoins::Unauthorized
+        when Net::HTTPNotFound
+          raise LocalBitcoins::NotFound
+      end
       hash = Hashie::Mash.new(JSON.parse(resp.body))
       raise Error.new(hash.error) if hash.error
       raise Error.new(hash.errors.join(',')) if hash.errors
       hash
-      # case resp
-      #   when Net::HTTPUnauthorized
-      #     raise LocalBitcoins::Unauthorized
-      #   when Net::HTTPNotFound
-      #     raise LocalBitcoins::NotFound
-      # end
-      #
-      # parse(resp)
-    end
-
-    def parse(resp)
-      JSON.parse(resp.body)
     end
   end
 end
